1ee6ab7a3e75e5cf1f8cbcc6cfb662d18e945a72
Cyrille Pierre <cyrille.pierre@inrae.fr>
2026-08-25 17:12:02 +0200

    fix: keep lateral_dev and course_dev when updating setpoint

--- src/path_following/path_following.cpp
+++ src/path_following/path_following.cpp
@@ -123,10 +123,22 @@ template<typename CommandType>
 typename PathFollowing<CommandType>::OnSetParametersResult
 PathFollowing<CommandType>::update_setpoint_(const std::vector<rclcpp::Parameter> & parameters)
 {
+  auto current_setpoint = setpoint_.load();
   setpoint_.store(
     {get_parameter_value<double>(parameters, "setpoint.desired_linear_speed"),
-     get_parameter_value_or<double>(parameters, "setpoint.desired_lateral_deviation", 0.0),
-     get_parameter_value_or<double>(parameters, "setpoint.desired_course_deviation", 0.0)});
+     get_parameter_value_or<double>(
+       parameters, "setpoint.desired_lateral_deviation", current_setpoint.lateral_deviation),
+     get_parameter_value_or<double>(
+       parameters, "setpoint.desired_course_deviation", current_setpoint.course_deviation)});
+
+  {
+    auto sp = setpoint_.load();
+    RCLCPP_INFO_STREAM(this->node_->get_logger(), "setpoint.linear_speed: " << sp.linear_speed);
+    RCLCPP_INFO_STREAM(
+      this->node_->get_logger(), "setpoint.lateral_deviation: " << sp.lateral_deviation);
+    RCLCPP_INFO_STREAM(
+      this->node_->get_logger(), "setpoint.course_deviation: " << sp.course_deviation);
+  }
   rcl_interfaces::msg::SetParametersResult result;
   result.successful = true;
   result.reason = "success";
